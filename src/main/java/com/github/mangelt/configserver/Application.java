6b95c4384557cda9d98622bdccab4ff9f5a5939f
Miguel Torres <miguelfreelancer56577@gmail.com>
2022-01-21 14:52:01 -0600

    Added configuration to run server config

--- src/main/java/com/github/mangelt/configserver/Application.java
+++ src/main/java/com/github/mangelt/configserver/Application.java
@@ -2,12 +2,14 @@ package com.github.mangelt.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class Lab11Application {
+@EnableConfigServer
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab11Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
