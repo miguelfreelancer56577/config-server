f7658b89f70ef26400221d4dd048835538387046
Miguel Torres <miguelfreelancer56577@gmail.com>
2022-01-24 12:22:41 -0600

    Added script files to start and stop instance.

---
+++ src/main/resources/aws/start-instance.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#declaring variables
+instanceName=$AWS_ENV'-instance'
+intanceValues=()
+instanceStateCode=""
+instanceStateName=""
+instanceId=""
+publicIpAddress=""
+
+isReserved(){
+
+	#checking if the resource already exists
+	rs=$(aws ec2 describe-instances --profile $AWS_PROFILE --query 'length(Reservations[].Instances[].[Tags[?Key==`Name`].Value][][?@==`'$instanceName'`][])')
+	if [ $rs != 0 ]; then
+		echo "######################### The resource already exists. #########################"
+		return 0
+	else
+		echo "######################### The resource doesnt exist. #########################"
+		return 1
+	fi
+}
+
+getInstanceInfo(){
+	
+	intanceValues=($(aws ec2 describe-instances \
+	--profile $AWS_PROFILE \
+	--query 'Reservations[*].Instances[*].{instanceStateCode: State.Code, instanceStateName: State.Name, publicIpAddress: PublicIpAddress, instanceId: InstanceId, instanceName: Tags[?Key==`Name`].Value}[?instanceName[0]==`'$instanceName'`][][instanceId, instanceStateCode, instanceStateName]' \
+	--output text | 
+	tr -s '[:space:]' "\n"))
+	
+	instanceStateCode=${intanceValues[1]}
+	instanceStateName=${intanceValues[2]}
+	instanceId=${intanceValues[0]}
+	
+}
+isInstanceRuning(){
+	echo "######################### Instance State Name: $instanceStateName  #########################";
+	echo "######################### Instance State Code: $instanceStateCode  #########################";
+	#if status code is equal to 16 then this instance is running 
+	if [ $instanceStateCode == 16 ] 
+	then
+		return 0
+	else
+		return 1
+	fi 
+}
+
+startInstance(){
+	aws ec2 start-instances --instance-ids $instanceId
+}
+
+main(){
+
+	#checking if the resource already exists
+	isReserved
+	if [ $? == 0 ]; then
+		echo "######################### Getting instance information #########################"
+		getInstanceInfo
+		if [ $? == 0 ]; then
+			echo "######################### Is Instance running. #########################"
+			isInstanceRuning
+			if [ $? == 1 ] 
+			then
+				echo "######################### Starts Instance. #########################"
+				startInstance
+			else
+				echo "######################### The Instanceis already running. #########################"
+			fi
+		fi
+	fi
+}
+
+#calling main function
+main
